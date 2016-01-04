0c4a789669c7f304fdd80ee1960e46617b4f6c16
Yavor Georgiev <fealebenpae@gmail.com>
2016-01-04 15:30:23 +0200

    Implementation

---
+++ EmbeddedRuntime/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  EmbeddedRuntime
+//
+//  Created by Yavor Georgiev on 4.01.16 г..
+//  Copyright © 2016 г. NativeScript. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ EmbeddedRuntime/ViewController.h
@@ -0,0 +1,16 @@
+//
+//  ViewController.h
+//  EmbeddedRuntime
+//
+//  Created by Yavor Georgiev on 4.01.16 г..
+//  Copyright © 2016 г. NativeScript. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+- (IBAction)sayHello:(id)sender;
+
+@end
+
